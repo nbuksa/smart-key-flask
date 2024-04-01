ea8d42f32d9f753b203827e8f52f45c4803cbecb
nbuksa <buksa.nikolina@gmail.com>
2024-04-01 11:39:43 +0200

    add

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length
 from models.users import User
 from services.user_services import UserServices
+from datetime import datetime as dt
+
+
 
 
 
@@ -10,22 +14,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yekuc60n6T2lgumg7y40mIXhGQw9YKCul2f6OuTDm51e09dGNO5s9Wakh7aX58Iibvbq0Y37Q1Ns204YypgoNxn3610a1agfiUtA'
 
 class UserCreateForm(FlaskForm):
-    first_name = StringField('Ime')
-    last_name = StringField('Prezime')
-    pin_code = StringField('PIN')
+    first_name = StringField('Ime', validators=[DataRequired('Ovo polje je obavezno'), Length(max=50, message='Maksimalno 50 znakova')])
+    last_name = StringField('Prezime', validators=[DataRequired('Ovo polje je obavezno'), Length(max=100, message='Maksimalno 100 znakova')])
+    pin_code = StringField('PIN', validators=[DataRequired('Ovo polje je obavezno'), Length(max=4, message='Mora biti točno  4 znakova')])
     is_active = BooleanField('Aktivan?')
-    submit = SubmitField('Kreiraj')
+    submit = SubmitField('Snimi')
 
 
 
 # http://www.domena.hr/
 @app.route('/')
 def index():
-    user = UserServices().get_user()
-    user_form = UserCreateForm()
+    users = UserServices().get_users()
+    current_date = dt.now().strftime('%d.%m%Y. %H:%M:&S')
+    for user in users:
+        print(user)
 
-    
-    return render_template('index.html', user=user)
+    return render_template('index.html', users=users, time=current_date)
 
 
 # http://www.domena.hr/about
@@ -39,17 +44,39 @@ def about():
 def create_user():
     user_form = UserCreateForm()
 
-    if user_form.validate_on_submit():
+    if user_form.is_submitted():
+        
         user_first_name = user_form.first_name.data
         user_last_name = user_form.last_name.data
         user_pin_code = user_form.pin_code.data
         user_is_active = user_form.is_active.data
         UserServices.create_user(User(user_first_name, user_last_name, user_pin_code, user_is_active))
-        redirect(url_for('index'))
+        return redirect(url_for('index'))
 
     return render_template('create-user.html', form=user_form)
 
+@app.route('/edit-user/<:id>', methods=['GET', 'POST'])
+def edit_user(id):
+    user_form = UserCreateForm()
+
+    if request.method == 'GET':
+        user = UserServices.get_user(id)
+        user_form.first_name.data = user.first_name
+        user_form.last_name.data = user.last_name
+        user_form.pin_code.data = user.pin_code
+        user_form.is_active.data = user.is_active
+
+    if user_form.is_submitted():
+        
+        user_first_name = user_form.first_name.data
+        user_last_name = user_form.last_name.data
+        user_pin_code = user_form.pin_code.data
+        user_is_active = user_form.is_active.data
+        UserServices.create_user(User(user_first_name, user_last_name, user_pin_code, user_is_active))
+        return redirect(url_for('index'))
+
+    return render_template('edit-user.html', form=user_form)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8888)
--- services/user_services.py
+++ services/user_services.py
@@ -1,20 +1,42 @@
 from models.users import User
 from repositories.file_repository import FileRepo
+from typing import List
 
 
 class UserServices:
 
     @classmethod
     def create_user(cls, user: User):
+        return FileRepo.save_to_file(user.__dict__)
+
+    @classmethod
+    def get_users(cls) -> List[User]:
+        users_from_file = FileRepo.read_from_file()
+        users_list = []
+        for user in users_from_file:
+
+            if user['is_active'] == True:
+                users_list.append(User(first_name=user['first_name'], last_name=user['last_name'], pin_code=user['pin_code'], is_active=True, id = user['id']))
+            else:
+                users_list.append(User(first_name=user['first_name'], last_name=user['last_name'], pin_code=user['pin_code'], is_active=True, id = user['id']))
+        return users_list
+    
+    @classmethod
+    def get_user(cls, id) -> User:
+        users_from_file = FileRepo.read_from_file()
+        users_list = []
+        if users_from_file != None:
+            for user in users_from_file:
+    
+                if user['is_active'] == True:
+                    return (User(first_name=user['first_name'], last_name=user['last_name'], pin_code=user['pin_code'], is_active=True, id = user['id']))
+                else:
+                    return (User(first_name=user['first_name'], last_name=user['last_name'], pin_code=user['pin_code'], is_active=True, id = user['id']))
+
+
+    @classmethod
+    def update_user(cls, user: User):
         return FileRepo.save_to_file(str(user))
-
-    @classmethod
-    def get_user(cls) -> User:
-        return User(first_name='Pero', last_name='Peric', pin_code='1234', is_active=False)
-
-    @classmethod
-    def update_user(cls):
-        pass
     
     @classmethod
     def delete_user(cls):
